c4e77fd495dbd8dc5b9ccc041dbe3d2fd7fb9608
mtatto <michaeltatto@hotmail.com>
2014-03-12 08:54:07 -0700

    https://github.com/mtatto/Utilities.git

---
+++ XmlParser/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace XmlParser
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            XmlReader reader = XmlReader.Create(new StringReader(Resource1.String1));
+            XElement root1 = XElement.Load(reader);
+
+            XmlReader reader2 = XmlReader.Create(new StringReader(Resource1.String1));
+            XElement root2 = XElement.Load(reader2);
+
+            XmlReader reader3 = XmlReader.Create(new StringReader(Resource1.String1));
+            XElement root3 = XElement.Load(reader3);
+
+            Console.WriteLine("-------Orginal XML File------");
+            Console.WriteLine(root1);
+            root2.UpdateSingleXmLElement("//lab[@type='hemo']/foo", "UpdatedValue");
+            Console.WriteLine();
+            Console.WriteLine("-------Single Value Changed XML File------");
+            Console.WriteLine(root2);
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("-------Orginal XML File------");
+            Console.WriteLine(root1);
+
+            var xpathsValues = new List<Tuple<string, string>>
+            {
+                new Tuple<string, string>("//lab[@type='hemo']/bar", "ABCDEF"),
+                new Tuple<string, string>("//lab[@type='hemo1']/order", "COOL"),
+                new Tuple<string, string>("//lab[@type='hemo2']/foo", "BAAAAAA")
+            };
+
+            root2.UpdateXmLElements(xpathsValues);
+            Console.WriteLine(root3);
+
+            Console.WriteLine("-------Compare XML Files------");
+            bool areEqual = String.Equals(root1.ToString(), root2.ToString(), StringComparison.Ordinal);
+            Console.WriteLine("{0}and {1} are equal? {2}", "root1", "root2", areEqual);
+
+            //Another way to compare
+            Console.WriteLine(XNode.DeepEquals(root1, root2));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ XmlParser/XmlUtilities.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace XmlParser
+{
+    public static class XmlUtilities
+    {
+        public static bool UpdateSingleXmLElement(this XElement xElement, string xPath, string newValue)
+        {
+            bool returnValue = false;
+            try
+            {
+                XElement el = xElement.XPathSelectElement(xPath);
+                if (el != null)
+                {
+                    el.SetValue(newValue);
+                    returnValue = true;
+                }
+            }
+            catch (Exception)
+            {
+                //TODO: Log This
+                returnValue = false;
+            }
+            return returnValue;
+        }
+
+        public static void UpdateXmLElements(this XElement xElement, List<Tuple<string, string>> xPathsAndNewValues)
+        {
+            try
+            {
+                foreach (var xPathAndNewValue in xPathsAndNewValues)
+                {
+                    XElement el = xElement.XPathSelectElement(xPathAndNewValue.Item1);
+                    if (el != null)
+                    {
+                        el.SetValue(xPathAndNewValue.Item2);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                //TODO: Log This?
+            }
+        }
+   
+    }
+}
